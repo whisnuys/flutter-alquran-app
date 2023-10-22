0a561539a093886eb01fcef258630a5c26cc4c6a
whisnuys <whisnusaputra30@gmail.com>
2023-10-22 12:31:31 +0700

    change primary color

--- lib/common/contants.dart
+++ lib/common/contants.dart
@@ -1,8 +1,9 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
-  static const Color primary = Color(0xffDA8856);
-  static const Color black = Color(0xff292C29);
+  static const Color primary = Color(0xff672CBC);
+  static const Color black = Color(0xff240F4F);
+  static const Color grey = Color(0xff8789A3);
   static const Color white = Color(0xffFFFFFF);
   static const Color brown = Color(0xffBBC4CE);
   static const Color black60 = Color(0xff292C29);
